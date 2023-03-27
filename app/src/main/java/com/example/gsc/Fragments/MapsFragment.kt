38c12e7bd7db6c37af6237f2b8a592a23d802222
Mehul Gupta <mehulgupta2110@gmail.com>
2023-03-28 02:55:56 +0530

    updated MapsFragment.kt, HelpActivity.kt, edited map_style.json

--- app/src/main/java/com/example/gsc/Fragments/MapsFragment.kt
+++ app/src/main/java/com/example/gsc/Fragments/MapsFragment.kt
@@ -1,52 +1,69 @@
 package com.example.gsc.Fragments
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.media.Image
 import androidx.fragment.app.Fragment
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.FrameLayout
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.navigation.fragment.findNavController
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import com.example.gsc.HelpActivity
 import com.example.gsc.R
 import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationServices
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.*
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.RectangularBounds
+import com.google.android.libraries.places.api.model.TypeFilter
+import com.google.android.libraries.places.api.net.FindAutocompletePredictionsRequest
 import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.maps.android.SphericalUtil
+import kotlin.math.cos
 
-class MapsFragment : Fragment() {
+class MapsFragment : Fragment(),OnMapReadyCallback{
     private lateinit var bottomSheetBehavior: BottomSheetBehavior<FrameLayout>
     private lateinit var bottomSheetView: FrameLayout
     private lateinit var map:GoogleMap
-    @SuppressLint("MissingPermission")
-    private val callback = OnMapReadyCallback { googleMap ->
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
-        val sydney = LatLng(-34.0, 151.0)
-        googleMap.addMarker(MarkerOptions().position(sydney).title("Your current location"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-        googleMap.uiSettings.apply {
-            isZoomControlsEnabled = true
-            isMyLocationButtonEnabled = true
-        }
-    }
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+//    @SuppressLint("MissingPermission")
+//    private val callback = OnMapReadyCallback { googleMap ->
+//        /**
+//         * Manipulates the map once available.
+//         * This callback is triggered when the map is ready to be used.
+//         * This is where we can add markers or lines, add listeners or move the camera.
+//         * In this case, we just add a marker near Sydney, Australia.
+//         * If Google Play services is not installed on the device, the user will be prompted to
+//         * install it inside the SupportMapFragment. This method will only be triggered once the
+//         * user has installed Google Play services and returned to the app.
+//         */
+//        val sydney = LatLng(-34.0, 151.0)
+//        googleMap.addMarker(MarkerOptions().position(sydney).title("Your current location"))
+//        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+//        googleMap.uiSettings.apply {
+//            isZoomControlsEnabled = true
+//            isMyLocationButtonEnabled = true
+//        }
+//    }
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -54,21 +71,132 @@ class MapsFragment : Fragment() {
     ): View? {
         val view=inflater.inflate(R.layout.fragment_maps, container, false)
         bottomSheetView=view.findViewById(R.id.frame_modal)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
+
         return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
-        mapFragment?.getMapAsync(callback)
+        mapFragment?.getMapAsync(this)
         bottomSheetBehavior = BottomSheetBehavior.from(bottomSheetView)
         bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
         bottomSheetBehavior.isDraggable = false
         bottomSheetBehavior.isHideable = false
+        view.findViewById<Button>(R.id.help_button).setOnClickListener {
+            startActivity(Intent(requireContext(),HelpActivity::class.java))
+        }
+        view.findViewById<ImageView>(R.id.iv_profile).setOnClickListener {
+            findNavController().navigate(R.id.profileFragment)
+        }
     }
+
+    @SuppressLint("MissingPermission")
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+        map.isMyLocationEnabled = true
+        val style = MapStyleOptions.loadRawResourceStyle(requireContext(), R.raw.map_style)
+
+        map.clear()
+//        map.setOnMyLocationButtonClickListener()
+        fusedLocationClient.getCurrentLocation(
+            LocationRequest.PRIORITY_HIGH_ACCURACY,
+            null
+        ).addOnSuccessListener { location ->
+            // Check if the location is not null
+            if (location != null) {
+                // Move the camera to the user's current location
+                val cameraPosition = CameraPosition.Builder()
+                    .target(LatLng(location.latitude, location.longitude))
+                    .zoom(15f)
+                    .build()
+                map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition), 2000, null)
+                map.setMapStyle(style)
+
+//                if (!Places.isInitialized()) {
+//                    Places.initialize(requireContext(), "AIzaSyBWJu44Js9xy8ZFUy1wAsxfSWmgbrtEv18")
+//                }
+//
+//                val placesClient = Places.createClient(requireContext())
+//                val radius = 5000 // in meters
+//                val type = "veterinary_care"
+//                val apiKey="AIzaSyBWJu44Js9xy8ZFUy1wAsxfSWmgbrtEv18"
+//
+//                val bounds = LatLngBounds.builder()
+//                    .include(
+//                        LatLng(
+//                            22.718137699108006 - radius / 111000.0,
+//                            75.90834320343207 - radius / (111000.0 * cos(location.latitude * Math.PI / 180.0))
+//                        )
+//                    )
+//                    .include(
+//                        LatLng(
+//                            22.718137699108006 + radius / 111000.0,
+//                            75.90834320343207 + radius / (111000.0 * cos(location.latitude * Math.PI / 180.0))
+//                        )
+//                    )
+//                    .build()
+//                val queue=Volley.newRequestQueue(requireContext())
+//                val url =
+//                    "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=22.718137699108006,75.90834320343207&radius=$radius&type=$type&key=$apiKey"
+//                val jsonObjectRequest: JsonObjectRequest =JsonObjectRequest(
+//                    Request.Method.GET,
+//                    url,
+//                    null,
+//                    {
+//                        val newsJsonArray=it.getJSONArray("results")
+//                        for (i in 0 until newsJsonArray.length()){
+//                            val newsJSONObject=newsJsonArray.getJSONObject(i)
+//                            val location =newsJSONObject.getJSONObject("geometry").getJSONObject("location")
+//                            val lat=location.getDouble("lat")
+//                            val lng=location.getDouble("lng")
+//                            val name=newsJSONObject.getString("name")
+//                            map.addMarker(MarkerOptions()
+//                                .position(LatLng(lat,lng))
+//                                .title(name)
+//                            )
+//                        }
+//                    },
+//                    {
+//
+//                    })
+//                queue.add(jsonObjectRequest)
+            }
+            map.uiSettings.apply {
+                isZoomControlsEnabled = false
+                isRotateGesturesEnabled = false
+                isTiltGesturesEnabled = false
+//                isZoomGesturesEnabled = false
+            }
+        }
+
+//    @SuppressLint("MissingPermission")
+//    override fun onMyLocationButtonClick(): Boolean {
+//        fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+//            // Check if the location is not null
+//            if (location != null) {
+//                // Move the camera to the user's current location
+//                val cameraPosition = CameraPosition.Builder()
+//                    .target(LatLng(location.latitude, location.longitude))
+//                    .zoom(15f)
+//                    .build()
+//                map.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
+//            } else {
+//                // If the location is null, show a toast message
+//                Toast.makeText(
+//                    requireContext(),
+//                    "Unable to get current location",
+//                    Toast.LENGTH_SHORT
+//                ).show()
+//            }
+//        }
+//        return true
+//    }
 
 //    override fun onMapReady(googleMap: GoogleMap) {
 //        map=googleMap
 //
 //    }
-}
+    }
+    }
--- app/src/main/java/com/example/gsc/HelpActivity.kt
+++ app/src/main/java/com/example/gsc/HelpActivity.kt
@@ -19,67 +19,10 @@ import com.google.android.gms.location.LocationServices
 import kotlin.properties.Delegates
 
 class HelpActivity : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_help)
-
-        var latitude=intent.getDoubleExtra("latitude",0.0)
-        var longitude = intent.getDoubleExtra("longitude",0.0)
-
-        val imageView=findViewById<ImageView>(R.id.iv_map)
-        Glide.with(this)
-            .load(getStaticMapUrl(latitude,longitude))
-            .into(imageView)
-
-        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            // Retrieve the user's current location using the FusedLocationProviderClient API
-            val fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
-            fusedLocationProviderClient.lastLocation.addOnSuccessListener { location: Location? ->
-                // Construct the Places API request URL
-                val radius = 10000 // in meters
-                val type = "veterinary"
-                val url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${location?.latitude},${location?.longitude}&radius=$radius&type=$type&key=AIzaSyCpr0ZyHFFcpbnTSVD7434Oc9m01BzBhEo"
-
-                // Send the request to the Places API using Volley or OkHttp
-                val requestQueue = Volley.newRequestQueue(this)
-                val request = JsonObjectRequest(
-                    Request.Method.GET,
-                    url,
-                    null,
-                    { response ->
-                        // Parse the response and extract the necessary information about the nearby veterinary hospitals
-                        val results = response.getJSONArray("results")
-                        for (i in 0 until results.length()) {
-                            val result = results.getJSONObject(i)
-
-                            val name = result.getString("name")
-                            Log.d("address1", name.toString())
-                            val address = result.getString("vicinity")
-                            val location = result.getJSONObject("geometry").getJSONObject("location")
-                            val latitude1 = location.getDouble("lat")
-                            val longitude1 = location.getDouble("lng")
-                            // Do something with the information, such as adding markers to a map or displaying them in a list
-                        }
-                    },
-                    { error ->
-                        // Handle the error
-                    }
-                )
-                requestQueue.add(request)
-            }
-        } else {
-            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION,android.Manifest.permission.ACCESS_COARSE_LOCATION), 1)
-        }
-    }
-
-    private fun getStaticMapUrl(latitude: Double, longitude: Double): String {
-        val apiKey = "AIzaSyAoFVFGd4v0q28Yx-WdreQTelZt2MZCmuQ" // Replace with your API key
-        val width = Resources.getSystem().displayMetrics.widthPixels
-        val size = "200x${width}" // Set the size of the image
-        val color = "#FFB3C1"
-
-        return "https://maps.googleapis.com/maps/api/staticmap?center=$latitude,$longitude&zoom=15&size=$size&key=$apiKey&markers=icon:https://appurl.io/DtWgrrStFQ%7Ccolor:blue%7Clabel:S%7C$latitude,$longitude%7C"
     }
 }
